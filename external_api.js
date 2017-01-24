39918c96031cda498b323b5efa378c4e4bdb18c4
Abdusamad <ZKWFZPJ@corp.bankofamerica.com>
2017-01-24 14:41:53 -0600

    external api

--- external_api.js
+++ external_api.js
@@ -1,14 +1,35 @@
-const request = require('request-promise');
+const rp = require('request-promise');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
-const options = {
-	method: 'GET',
-	uri: ''
-}
+const app = express();
 
-request(options)
-	.then((response) => {
+app.engine('.hbs', exphbs({
+	defaultLayout: 'main',
+	extname: '.hbs',
+	layoutsDir: path.join(__dirname, 'views/layouts')
+}))
 
+// app.set('view engine', '.hbs');
+// app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+	rp({
+		uri: 'http://google.com',
+		// qs: {
+		// 	q: req.params.city,
+		// 	apikey: '1OL537IAaXcSQ7LXZ7CGVuBbzUz5KSnl'
+		// },
+		json: true
+	}).then((data) => {
+		//res.render('index', data);
+		console.log(data);
+	}).catch((err) => {
+		console.log('--------------- ERROR ------------------------');
+		console.log(err);
+		// res.render('error');
 	})
-	.catch((err) => {
+})
 
-	});
+app.listen(3000);
